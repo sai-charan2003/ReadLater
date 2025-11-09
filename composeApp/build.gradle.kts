b9d304801c82fedd78eff72be35ff85bacbe8069
sai-charan2003 <saicharannallapaneni@gmail.com>
2025-11-09 13:18:53 -0500

    Github actions fix

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -167,6 +167,7 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    buildToolsVersion  = "34.0.0"
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
